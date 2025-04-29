2ed81b7a42f12fcdc58c0b7dcfae47940e7e76ea
Roger0222 <20211113@nbsc.edu.ph>
2025-04-30 00:53:12 +0800

    minor changes on vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     legacy()
   ],
-  base:"/it35-lab",
+  base: "/it35-lab/", // Important for GitHub Pages
   test: {
     globals: true,
     environment: 'jsdom',
