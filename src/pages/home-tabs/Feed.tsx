1cb6f58a51a89f3e36e0a042265dcbc814829068
Roger0222 <20211113@nbsc.edu.ph>
2025-05-21 20:32:51 +0800

    minor change on feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -12,30 +12,18 @@ import FeedContainer from '../../components/FeedContainer';
 
 const Feed: React.FC = () => {
   return (
-    <IonPage>
-      {/* Header */}
-      <IonHeader style={{ backgroundColor: '#232323' }}>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton color="light"></IonMenuButton>
-          </IonButtons>
-          <IonTitle style={{ color: '#FFFFFF', fontSize: '26px', fontWeight: '700' }}>
-            Feed
-          </IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      
-        {/* Styled Feed Container */}
-        <div style={{ 
-          marginTop: '20px', 
-          padding: '20px', 
-          borderRadius: '10px', 
-          backgroundColor: '#333', 
-          boxShadow: '0 4px 8px rgba(0,0,0,0.3)' 
-        }}>
-          <FeedContainer />
-        </div>
-    </IonPage>
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Search</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+        </IonContent>
+      </IonPage>
   );
 };
 
