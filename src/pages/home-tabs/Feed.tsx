cad59b3aa245a246dea1d7218b22853d3b9d1ab6
Roger0222 <20211113@nbsc.edu.ph>
2025-05-21 21:04:55 +0800

    minor changes

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -12,22 +12,7 @@ import FeedContainer from '../../components/FeedContainer';
 
 const Feed: React.FC = () => {
   return (
-    <IonPage>
-      {/* Header */}
-      <IonHeader style={{ backgroundColor: '#232323' }}>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton color="light">
-            </IonMenuButton>
-          </IonButtons>
-          <IonTitle style={{ color: '#FFFFFF', fontSize: '26px', fontWeight: '700' }}>
-            Feed
-          </IonTitle>
-        </IonToolbar>
-      </IonHeader>
-          <FeedContainer />
-
-    </IonPage>
+    <FeedContainer/>
   );
 };
 
